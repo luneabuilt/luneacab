b6242ded3f4769c6a97d2d857096cd67a3624d03
A H Md Abdullah <abuhind@As-MacBook-Air.local>
2026-03-17 12:52:30 +0530

    fix firebase admin env config

--- server/index.ts
+++ server/index.ts
@@ -3,7 +3,6 @@ import express, { type Request, Response, NextFunction } from "express";
 import { registerRoutes } from "./routes";
 import { serveStatic } from "./static";
 import { createServer } from "http";
-import http from "http";
 import { Server } from "socket.io";
 import cors from "cors";
 
