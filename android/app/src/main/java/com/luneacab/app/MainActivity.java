b79838fb141d1c8d4854a104a09cc7d44a2db7dc
A H Md Abdullah <abuhind@As-MacBook-Air.local>
2026-04-23 02:35:57 +0530

    update

--- android/app/src/main/java/com/luneacab/app/MainActivity.java
+++ android/app/src/main/java/com/luneacab/app/MainActivity.java
@@ -1,5 +1,32 @@
-package com.luneacab.app;
+package your.package.name;
 
+import android.os.Bundle;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    createNotificationChannel();
+  }
+
+  private void createNotificationChannel() {
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      NotificationChannel channel = new NotificationChannel(
+        "default",
+        "Default Channel",
+        NotificationManager.IMPORTANCE_HIGH
+      );
+
+      channel.setDescription("Ride Notifications");
+
+      NotificationManager manager = getSystemService(NotificationManager.class);
+      manager.createNotificationChannel(channel);
+    }
+  }
+}
